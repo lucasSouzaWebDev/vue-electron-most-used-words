9f2b1802ad8ca2f132893f332d03a3bf361c62cc
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2022-09-10 16:48:09 -0300

    Creating graphic interface.

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input label="Selecione as legendas" prepend-icon="mdi-message-text" append-outer-icon="mdi-send" outlined multiple chips v-model="files" @click:append-outer="processSubtitles" />
+    </v-form>
+    <div class="pills">
+      <PillComponent
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import PillComponent from "./PillComponent";
+
+export default {
+  components: {
+    PillComponent,
+  },
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [
+        {
+          name: "you",
+          amount: 900,
+        },
+        {
+          name: "he",
+          amount: 853,
+        },
+        {
+          name: "i",
+          amount: 1234,
+        },
+      ],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
