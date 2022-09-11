74532d61c900843f90a5aef29696f6cd2baf1997
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2022-09-11 11:17:42 -0300

    Developing backend processing words.

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,28 +1,38 @@
 const { ipcMain } = require('electron')
 
+const pathToRows = require('pathToRows')
+const prepareData = require('prepareData')
+
 ipcMain.on('process-subtitles', (event, paths) => {
     console.log(paths);
 
-    event.reply('process-subtitles', [
-        {
-            name: "you",
-            amount: 901,
-        },
-        {
-            name: "he",
-            amount: 853,
-        },
-        {
-            name: "she",
-            amount: 457,
-        },
-        {
-            name: "our",
-            amount: 687,
-        },
-        {
-            name: "i",
-            amount: 1234,
-        },
-    ])
+    pathToRows
+        .then(rows => prepareData(rows))
+        .then(words => console.log(words))
+        .then(() => {
+            event.reply('process-subtitles', [
+                {
+                    name: "you",
+                    amount: 901,
+                },
+                {
+                    name: "he",
+                    amount: 853,
+                },
+                {
+                    name: "she",
+                    amount: 457,
+                },
+                {
+                    name: "our",
+                    amount: 687,
+                },
+                {
+                    name: "i",
+                    amount: 1234,
+                },
+            ])
+        })
+
+
 })
